070b621667f282c6c8813ebaf642217ff62e3861
Dipankar Jana <dipankar@okcredit.in>
2024-11-23 19:00:19 +0530

    Add tag relationships

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
 
 class Job extends Model {
   protected $table = 'job_listings';
@@ -13,4 +12,12 @@ class Job extends Model {
     'title',
     'salary'
   ];
+
+  public function employer() {
+      return $this->belongsTo(Employer::class);
+  }
+
+  public function tags() {
+      return $this->belongsToMany(Tag::class, foreignPivotKey: 'job_listing_id');
+  }
 }
