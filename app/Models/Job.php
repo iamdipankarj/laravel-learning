571dc5c5a2fb91cf87889f53586fab5748873da6
Dipankar Jana <dipankar@okcredit.in>
2024-11-23 17:48:58 +0530

    Add employer

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,11 +1,13 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
 class Job extends Model {
   protected $table = 'job_listings';
+  use HasFactory;
 
   protected $fillable = [
     'title',
