a431e1d136f1934cf64dae31cc5254de6d6de80a
Dipankar Jana <dipankar@okcredit.in>
2024-11-26 11:44:50 +0530

    Add route controller

--- routes/web.php
+++ routes/web.php
@@ -1,70 +1,36 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
 
 Route::get('/', function () {
   return view('home');
 });
 
-Route::get('/jobs', function () {
-  $jobs = Job::with('employer')->latest()->simplePaginate(10);
-  return view('jobs.index', [
-    'jobs' => $jobs
-  ]);
-});
+Route::view('/', 'home');
 
-Route::post('/jobs', function () {
-  request()->validate([
-    'title' => ['required', 'min:3'],
-    'salary' => 'required'
-  ]);
-  Job::create([
-    'title' => request('title'),
-    'salary' => request('salary'),
-    'employer_id' => 1,
-  ]);
-  return redirect('/jobs');
-});
+//Route::controller(JobController::class)->group(function () {
+//  Route::get('/jobs', 'index');
+//
+//  // Create
+//  Route::get('/jobs/create', 'create');
+//
+//  // Show
+//  Route::get('/jobs/{job}', 'show');
+//
+//  // Store
+//  Route::post('/jobs', 'store');
+//
+//  // Edit
+//  Route::get('/jobs/{job}/edit', 'edit');
+//
+//  // Update
+//  Route::patch('/jobs/{job}', 'update');
+//
+//  // Delete
+//  Route::delete('/jobs/{job}', 'destroy');
+//});
 
-Route::get('/jobs/create', function () {
-  return view('jobs.create');
-});
+Route::resource('jobs', JobController::class);
 
-Route::get('/jobs/{id}', function ($id) {
-  $job = Job::find($id);
-  return view('jobs.show', [
-    'job' => $job
-  ]);
-});
-
-Route::patch('/jobs/{id}', function ($id) {
-  request()->validate([
-    'title' => ['required', 'min:3'],
-    'salary' => 'required'
-  ]);
-
-  $job = Job::findOrFail($id);
-  $job->update([
-    'title' => request('title'),
-    'salary' => request('salary')
-  ]);
-
-  return redirect('/jobs/' . $job->id);
-});
-
-Route::delete('/jobs/{id}', function ($id) {
-  Job::findOrFail($id)->delete();
-
-  return redirect('/jobs');
-});
-
-Route::get('/jobs/{id}/edit', function ($id) {
-  return view('jobs.edit', [
-    'job' => Job::find($id)
-  ]);
-});
-
-Route::get('/contact', function () {
-  return view('contact');
-});
+Route::view('/contact', 'contact');
